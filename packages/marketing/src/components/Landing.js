018cff32b09d1e68e10050e74078e7796708f860
cj <49462194+cj-yourjourney@users.noreply.github.com>
2025-03-05 14:27:12 -0800

    updated the copyright

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -14,7 +14,7 @@ import { Link } from 'react-router-dom';
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {'Copyright 123333© '}
       <MaterialLink component={Link} to="/" color="inherit">
         Your Website
       </MaterialLink>{' '}
