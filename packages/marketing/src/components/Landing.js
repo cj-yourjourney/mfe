90f639a0e986708959fe7ee2cc8965787815b08d
cj-yourjourney <49462194+cj-yourjourney@users.noreply.github.com>
2025-03-06 15:18:44 -0800

    Update Landing.js

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -127,7 +127,7 @@ export default function Album() {
                       Heading
                     </Typography>
                     <Typography>
-                      This is a media card. You can use this section to describe
+                      This is a media card change from the github. You can use this section to describe
                       the content.
                     </Typography>
                   </CardContent>
