990044a1423e428b8a34feccff72a9920d10f0a0
cj <49462194+cj-yourjourney@users.noreply.github.com>
2025-03-07 14:47:49 -0800

    added production class name

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,24 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from '../components/MarketingApp'
 import Header from '../components/Header'
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
+
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+})
+
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Hi there! now!!!</h1>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <h1>Hi there! now!!!</h1>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   )
 }
