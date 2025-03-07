63448928709d1d194d58dd59fc515ae083bc96cb
cj <49462194+cj-yourjourney@users.noreply.github.com>
2025-03-07 13:55:31 -0800

    removed the text in app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -7,7 +7,7 @@ export default () => {
   return (
     <BrowserRouter>
       <div>
-        <h1>Hi there! now!!! container-dev branch</h1>
+        <h1>Hi there! now!!!</h1>
         <Header />
         <MarketingApp />
       </div>
