ee103924348c8e707ca6772404b5d35b3ce91eaf
cj <49462194+cj-yourjourney@users.noreply.github.com>
2025-03-07 13:53:47 -0800

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from '../components/MarketingApp'
+import Header from '../components/Header'
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there! now!!! container-dev branch</h1>
-      <br />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Hi there! now!!! container-dev branch</h1>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   )
 }
